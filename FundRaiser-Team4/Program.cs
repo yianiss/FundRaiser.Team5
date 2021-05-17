dd4608904b08c5682261f4a18e7ff1d4c544d05a
Giannis Karopoulos <gkaropoulos@athtech.gr>
2021-05-17 20:13:35 +0100

    Add project files.

---
+++ FundRaiser-Team4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FundRaiser_Team4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

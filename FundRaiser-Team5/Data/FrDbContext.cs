e3ba90cdea1f2b29b0c9eccaf6831d7279abd8a5
theodtasia <theodtasia@csd.auth.gr>
2021-05-20 16:42:22 +0300

    first classes at DbContext

---
+++ FundRaiser-Team5/Data/FrDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundRaiser_Team5.Data
+{
+    class FrDbContext : DbContext
+    {
+        public DbSet<User> Users;
+        public DbSet<Project> Projects;
+        public DbSet<StatusUpdate> StatusUpdates;
+    }
+}

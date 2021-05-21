237e5b14c883a6dbfb2d6b31c2252e814e92c50f
Giannis Karopoulos <gkaropoulos@athtech.gr>
2021-05-21 10:21:52 +0100

    add Backer and Creator classes change the User class to Interface

--- FundRaiser-Team5/Model/Backer.cs
+++ FundRaiser-Team5/Model/Backer.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace FundRaiser_Team5
+namespace FundRaiser_Team5.Model
 {
-    public interface IUser
+    public class Backer : IUser
     {
+        public int BackerId { get; set; }
+        public List<Project> Projects { get; set; }
         public int UserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-
     }
 }

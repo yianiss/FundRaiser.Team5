0d0bb87ad235f46a14eee3f56224d43ba948a07e
theodtasia <theodtasia@csd.auth.gr>
2021-05-24 23:25:11 +0300

    put comments to services/add right DBcontext

---
+++ FundRaiser-Team5/Interfaces/IProjectInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundRaiser_Team5.Interfaces
+{
+    public interface IProjectInterface
+    { 
+    
+       // Task<List<Project>> GetProjectsAsync();
+        //Task<Project> CreateProjectAsync(OptionProject options);
+       // Task<User> GetProjectByIdAsync(int id);
+       // Task<int> DeleteProjectByIdAsync(int id);
+    }
+}
--- FundRaiser-Team5/Interfaces/IUserInterface.cs
+++ FundRaiser-Team5/Interfaces/IUserInterface.cs
@@ -9,9 +9,9 @@ namespace FundRaiser_Team5.Interfaces
 {
     public interface IUserInterface
     {
-        Task<List<User>> GetUsersAsync();
-        Task<User> CreateUserAsync(OptionUser options);
-        Task<User> GetUserByIdAsync(int id);
-        Task<int> DeleteUserByIdAsync(int id);
+       // Task<List<User>> GetUsersAsync();
+       // Task<User> CreateUserAsync(OptionUser options);
+       // Task<User> GetUserByIdAsync(int id);
+       // Task<int> DeleteUserByIdAsync(int id);
     }
 }

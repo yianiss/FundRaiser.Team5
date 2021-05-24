81d6d3ab459e8a44928e7a96a7e52a7d51f7c565
theodtasia <theodtasia@csd.auth.gr>
2021-05-24 16:52:50 +0300

    fixed errors and added migration

--- FundRaiser-Team5/Data/FrDbContext.cs
+++ FundRaiser-Team5/Data/FrDbContext.cs
@@ -4,12 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FundRaiser_Team5.Model;
 
 namespace FundRaiser_Team5.Data
 {
     class FrDbContext : DbContext
     {
-        public DbSet<User> Users;
+        public DbSet<IUser> Users;
         public DbSet<Project> Projects;
         public DbSet<StatusUpdate> StatusUpdates;
     }

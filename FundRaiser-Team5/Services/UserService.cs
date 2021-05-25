769cb05a304527ac759f0e803ac55f66baf023c0
Giannis Karopoulos <gkaropoulos@athtech.gr>
2021-05-25 11:55:27 +0100

    Create UserService

--- FundRaiser-Team5/Services/UserService.cs
+++ FundRaiser-Team5/Services/UserService.cs
@@ -1,4 +1,6 @@
-﻿using FundRaiser_Team5.Interfaces;
+﻿using FundRaiser_Team5.Data;
+using FundRaiser_Team5.Interfaces;
+using FundRaiser_Team5.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,32 @@ namespace FundRaiser_Team5.Services
 {
     public class UserService: IUserInterface
     {
-        private Crm
+        private readonly FrDbContext _context;
+        //private readonly ILogger<UserService> _logger;
+
+        public UserService(FrDbContext context, /*ILogger<UserService> logger*/)
+        {
+            _context = context;
+            //_logger = logger;
+        }
+        public Task<List<User>> GetUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> CreateUserAsync(OptionUser options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteUserByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

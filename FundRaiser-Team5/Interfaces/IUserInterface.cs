8aa42b67369ffe12a1ada9e7dfab7dded5f79f5e
Giannis Karopoulos <gkaropoulos@athtech.gr>
2021-05-30 12:35:47 +0100

    Add methods in IUserInterface

--- FundRaiser-Team5/Interfaces/IUserInterface.cs
+++ FundRaiser-Team5/Interfaces/IUserInterface.cs
@@ -9,9 +9,27 @@ namespace FundRaiser_Team5.Interfaces
 {
     public interface IUserInterface
     {
-       // Task<List<User>> GetUsersAsync();
-       // Task<User> CreateUserAsync(OptionUser options);
-       // Task<User> GetUserByIdAsync(int id);
-       // Task<int> DeleteUserByIdAsync(int id);
+         Task<User> CreateUserAsync(OptionUser options);
+         Task<List<User>> GetUsersAsync();
+         Task<User> GetUserByIdAsync(int id);
+
+        Task<User> GetUserAsync(OptionUser options);
+        Task<int> DeleteUserByIdAsync(int id);
+    }
+
+    public interface IUserService
+    {
+        public OptionUser CreateUser(OptionUser optionUser);
+
+        public List<OptionUser> ReadUser();
+        public OptionUser ReadUser(int UserId);
+        public List<OptionUser> ReadUser(OptionUser optionUser);
+        
+
+        public OptionUser UpdateUser(OptionUser optionUser, int id);
+
+        public bool DeleteUser(int id);
+
+    }
     }
 }

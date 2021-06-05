a5c2585f9b1afe85d1811c07e6b653570109cf24
Giannis Karopoulos <gkaropoulos@athtech.gr>
2021-06-05 07:56:11 +0100

    add migrations, publish to azure

--- FundRaiser.Team5.Persistence/Migrations/20210604231413_first.cs
+++ FundRaiser.Team5.Persistence/Migrations/20210604231413_first.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace FundRaiser.Team5.Persistence.Migrations
 {
-    public partial class test : Migration
+    public partial class first : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {

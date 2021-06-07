c9fa1c4fa1af4292689371e6b905f11166f2c413
Andreas Sempros <asempros@athtech.gr>
2021-06-07 19:12:16 +0300

    Add Image and video services

---
+++ FundRaiser.Team5.Core/Interfaces/IVideoPathService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FundRaiser.Team5.Core.Model;
+using FundRaiser_Team5.Options;
+
+namespace FundRaiser_Team5.Interfaces
+{
+    public interface IVideoPathService
+    {
+        Task<Result<List<OptionVideoPath>>> GetVideoPathsAsync();
+
+        Task<Result<OptionVideoPath>> CreateVideoPathAsync(OptionVideoPath options);
+
+        Task<Result<OptionVideoPath>> GetVideoPathByIdAsync(int id);
+
+        Task<Result<int>> DeleteVideoPathByIdAsync(int id);
+    }
+}

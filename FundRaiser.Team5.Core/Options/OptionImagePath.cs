bb034dc1e0c660cc971452a9ef0a2ecbf346ba1c
Andreas Sempros <asempros@athtech.gr>
2021-06-07 19:12:36 +0300

    Add option Image and Video path

---
+++ FundRaiser.Team5.Core/Options/OptionImagePath.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FundRaiser.Team5.Core.Entities;
+
+namespace FundRaiser_Team5.Options
+{
+    public class OptionImagePath
+    {
+        public int ImagePathId { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public string Image { get; set; }
+
+        public OptionImagePath() { }
+
+        public OptionImagePath(ImagePath imagePath)
+        {
+            if (imagePath != null)
+            {
+                ImagePathId = imagePath.ImagePathId;
+                ProjectId = imagePath.Project.ProjectId;
+                Image = imagePath.Image;
+            }
+        }
+
+        public ImagePath GetImagePath()
+        {
+            return new ImagePath
+            {
+                ImagePathId = ImagePathId,
+                Image = Image,
+            };
+        }
+    }
+}

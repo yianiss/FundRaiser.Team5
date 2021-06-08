28e4c2c360079c450886f5d9dde11d0d33c835fb
KonstantinosIoPapadopoulos <83708997+KonstantinosIoPapadopoulos@users.noreply.github.com>
2021-06-08 10:31:16 +0300

    Add Dtos

---
+++ FundRaiser.Team5.Core/Dto/Interfaces/IHomeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundRaiser_Team5.Dto.Interfaces
+{
+    interface IHomeService
+    {
+    }
+}

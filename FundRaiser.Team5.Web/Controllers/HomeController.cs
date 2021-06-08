8df4b33507b121ee484b5e9a5e7f58f052c1aa9a
Andreas Sempros <asempros@athtech.gr>
2021-06-08 15:23:14 +0300

    Add contact Us navigation item.

--- FundRaiser.Team5.Web/Controllers/HomeController.cs
+++ FundRaiser.Team5.Web/Controllers/HomeController.cs
@@ -24,6 +24,11 @@ namespace FundRaiser.Team5.Web.Controllers
             return View();
         }
 
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
